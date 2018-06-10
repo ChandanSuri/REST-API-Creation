155089d18f7b321a91ede795709eadfc7a6c8f54
ChandanSuri <csuri21296@gmail.com>
2018-06-10 14:59:43 +0530

    Some unsaved changes and Final App with Flask JWT Extended Added

--- Managed_Code/Resources/user.py
+++ Managed_Code/Resources/user.py
@@ -1,39 +1,27 @@
-import sqlite3
 from flask_restful import Resource, reqparse
-from Models.user import User
+from models.user import UserModel
 
-# clients interact with these resources (entities)...
 
 class UserRegister(Resource):
-
     parser = reqparse.RequestParser()
-    parser.add_argument("username",
-        type = str,
-        required = True,
-        help = "This field cannot be blank..."
-    )
-    parser.add_argument("password",
-        type = str,
-        required = True,
-        help = "This field cannot be blank..."
-    )
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
 
-    @classmethod
-    def post(cls):
+    def post(self):
+        data = UserRegister.parser.parse_args()
 
-        data = cls.parser.parse_args()
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
 
-        # no duplicate users with user names
-        if User.find_by_username(data["username"]):
-            return {"message": "A user with that username already exists..."}, 400
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
 
-        connection = sqlite3.connect(db_name)
-        cursor = connection.cursor()
-
-        insert_query = "INSERT INTO Users VALUES (NULL, ?, ?)"
-        cursor.execute(insert_query, (data["username"], data["password"]))
-
-        connection.commit()
-        connection.close()
-
-        return {"message" : "User Created Succesfully"}, 201 # create response Code
+        return {"message": "User created successfully."}, 201
